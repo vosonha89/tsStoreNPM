4b7dc8f738f379f5215b879c0cef6b41a2c6b012
Vo Son Ha <vosonha89@gmail.com>
2020-02-25 11:12:07 +0700

    add package lock

--- tsStore/src/tsStoreItem.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var TsStoreItem = /** @class */ (function () {
-    function TsStoreItem() {
-        this.storeItemId = '';
-    }
-    return TsStoreItem;
-}());
-exports.TsStoreItem = TsStoreItem;
